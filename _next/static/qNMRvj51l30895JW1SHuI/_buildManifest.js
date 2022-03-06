fc8e18a1c714db0fe64388226c8987aa4925d6c6
sullivantobias <sullivantobias@users.noreply.github.com>
2022-03-06 22:04:52 +0000

    deploy: 6993eca7c4cd6afef19d85911327d587b8189a46

---
+++ _next/static/qNMRvj51l30895JW1SHuI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/955229c28454e75a.css","static/chunks/pages/index-f138e2ba46d17ef8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
