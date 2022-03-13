8d101b961584560888285997ea4c9a1d78964d70
Sullivan Tobias <sullytobias@gmail.com>
2022-03-13 18:48:33 +0100

    Use Variants for Motion Framer

--- components/header/header.js
+++ components/header/header.js
@@ -9,7 +9,7 @@ import Title from "../title";
 import style from './header.module.scss';
 
 const Header = ({ planets, isBackDisplayed, planetId }) => {
-    const root = 'solar-system/planets/';
+    const root = '/solar-system/planets/';
 
     return <div className={ style.Header }>
         { isBackDisplayed &&
@@ -17,7 +17,7 @@ const Header = ({ planets, isBackDisplayed, planetId }) => {
                 <a className={ style.Link }> Back </a>
             </Link>
         }
-        { planets.map(({ name, id }) => <Link replace href={`${root}${name.toLowerCase()}`}>
+        { planets.map(({ name, id }) => <Link href={`${root}${name.toLowerCase()}`}>
                 <a className={
                     classNames(style.Title, { [style.isCurrent]: id === planetId }) }>
                     <Title tag={ 6 } title={ name } />
